884028b532a930a72862e0c5c23c883ae521ecc7
Tomas Mansilla Panozzo <98186505+Tmas-mnslla-pnzzo@users.noreply.github.com>
2023-11-29 21:24:52 -0300

    Update metodo_explicito.m

--- metodo_explicito.m
+++ metodo_explicito.m
@@ -1,5 +1,3 @@
-graphics_toolkit qt
-
 %constantes:
 T_ext=40;
 T_con=20;
@@ -10,52 +8,49 @@ l2=12;
 l3=5;
 l4=1;
 
-%W / (m K)
+%W/(m.K)
 k1=1;
 k2=0.400;
 k3=0.030;
 k4=0.250;
 
-% J/K.Kg;
+%J/K.Kg;
 c1=840; 
 c2=790; 
 c3=1600; 
 c4=1000;  
 
-%kg / m3
+%kg/m3
 P1=1800;
 P2=1000;
 P3=20;
 P4=600;
 
 %parametros:
-h=2;
-t_final=20000;
+
+factor=1;
+escala=6;
+t_final=5000;
 r1=1;
 r2=5;
 
-Dt=(h/r1);
-Dx=(h/r2);
+Dt=(factor/r1);
+Dx=(factor/r2);
 
 beta1=k1/(c1*P1)*100*100;
 beta2=k2/(c2*P2)*100*100;
 beta3=k3/(c3*P3)*100*100;
 beta4=k4/(c4*P4)*100*100;
 
-n1=(l1/Dx)*h;
-n2=(l2/Dx)*h;
-n3=(l3/Dx)*h;
-n4=(l4/Dx)*h;
+n1=l1*r2;
+n2=l2*r2;
+n3=l3*r2;
+n4=l4*r2;
 
 gamma1=((Dt/(Dx*Dx))*beta1);
 gamma2=((Dt/(Dx*Dx))*beta2);
 gamma3=((Dt/(Dx*Dx))*beta3);
 gamma4=((Dt/(Dx*Dx))*beta4);
-
-gamma1<0.5;
-gamma2<0.5;
-gamma3<0.5;
-gamma4<0.5;
 
 %funciones
 
@@ -89,6 +84,8 @@ function map=mapa(n,gam,U,T_con,t_final,Dt)
   map=V;
 endfunction
 
+%Desarollo
+
 M1=matriz_i(n1-1,gamma1);
 U1=[T_ext;ones(size(M1)(2)-1,1)*T_con];
 V=[U1];
@@ -98,30 +95,25 @@ for j=1:1:(1/Dt)*t_final
   V=[V U1];
 end
 
-Gf=6;
-
-Z1=V'; X1=[0: Dx/h: l1]; Y=[0: Dt*(2**Gf): t_final];
-
-U2=Z1(:,n1+1); Z2=mapa(n2,gamma2,U2,T_con,t_final,Dt); X2=[0: Dx/h: l2];
- 
-U3=Z2(:,n2+1); Z3=mapa(n3,gamma3,U3,T_con,t_final,Dt); X3=[0: Dx/h: l3];
-
-U4=Z3(:,n3+1); Z4=mapa(n4,gamma4,U4,T_con,t_final,Dt); X4=[0: Dx/h: l4];
+Z1=V'; X1=[0: Dx/factor: l1]; Y=[0: Dt*(2**escala): t_final];
+U2=Z1(:,n1+1); Z2=mapa(n2,gamma2,U2,T_con,t_final,Dt); X2=[0: Dx/factor: l2];
+U3=Z2(:,n2+1); Z3=mapa(n3,gamma3,U3,T_con,t_final,Dt); X3=[0: Dx/factor: l3];
+U4=Z3(:,n3+1); Z4=mapa(n4,gamma4,U4,T_con,t_final,Dt); X4=[0: Dx/factor: l4];
 
 Z_f1=horzcat(Z1,Z2); X_f1=horzcat(X1,X2+l1);
-
 Z_f2=horzcat(Z_f1,Z3); X_f2=horzcat(X_f1,X3+l2+l1);
 
 Z=horzcat(Z_f2,Z4); X=horzcat(X_f2,X4+l3+l2+l1);
 
-Zf=Z'(:,end);
+F=Z'(:,end);
 
-for gf=1:1:Gf
+for esc=1:1:escala
   Z(2:2:end,:)=[];
 end
 
+%Graficar
+
 subplot (2, 2, 3);
-  plot(X,Zf); title('Temperatura en el tiempo final. U(x,t_f)' ); xlabel ("Distancia [cm]"); ylabel ("Temperatura [°C]");
+  plot(X,F); title('Temperatura en el tiempo final. U(x,t_f)' ); xlabel ("Distancia [cm]"); ylabel ("Temperatura [°C]");
 subplot (2, 2, 1);
   waterfall(X,Y,Z); title('Temperatura U(x,t)' ); xlabel ("Distancia [cm]"); ylabel ("Tiempo [s]"); zlabel("Temperatura [°C]");
-
